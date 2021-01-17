44f862394808cfe573931081bf9e216423883bdb
Prithwiraj Bose <sribasu@gmail.com>
2021-01-17 06:24:36 +0530

    Final template, inclusion of scripts

---
+++ ui/resources/script.js
@@ -0,0 +1,64 @@
+var API_PATH = 'http://localhost:8080';
+
+var handleError = function(xhr, status, err) {
+    alert(JSON.stringify(xhr));
+};
+
+var showLoading = function() {
+
+};
+
+var hideLoading = function() {
+
+};
+
+var xhr = function(userConfig) {
+    if (_.isNil(userConfig) || _.isNil(userConfig.url)) {
+        throw 'Required config missing';
+    }
+    var isPost = _.toLower(_.get(userConfig, 'method', 'get')) == 'post';
+    var reqData = null;
+    if (!_.isNil(userConfig.form) && $(userConfig.form).length > 0) {
+        reqData = $.serialize(userConfig.form);
+    } else if (_.isObject(userConfig.data)) {
+        reqData = JSON.serialize(userConfig.data);
+    } else {
+        reqData = userConfig.data;
+    }
+    var config = {
+        url: (userConfig.url.indexOf('http://') !== -1 || userConfig.url.indexOf('https://') !== -1) ? userConfig.url : API_PATH + userConfig.url,
+        method: isPost ? 'post' : 'get',
+        dataType: _.get(userConfig, 'dataType', 'json'),
+        data: reqData,
+        contentType: _.get(userConfig, 'contentType', (_.isObject(userConfig.data) ? 'application/json' : 'application/x-www-form-urlencoded; charset=UTF-8')),
+        crossDomain: true,
+        timeout: _.get(userConfig, 'timeout', 0),
+        success: function(resp, status, xhr) {
+            if (resp && !_.isNil(resp.success) && resp.success === true) {
+                if (_.isFunction(userConfig.callback)) {
+                    userConfig.success(resp, status, xhr);
+                }
+            } else {
+                handleError(xhr, status, resp.error);
+            }
+        },
+        error: function(xhr, status, err) {
+            handleError(xhr, status, err);
+            if (_.isFunction(userConfig.error)) {
+                userConfig.error(xhr, status, err);
+            }
+        },
+        beforeSend: function(xhr, settings) {
+            if ($.active <= 0) {
+                showLoading();
+            }
+        },
+        complete: function(xhr, status) {
+            if ($.active == 1) {
+                hideLoading();
+            }
+        }
+    };
+
+    return $.ajax(config);
+};
